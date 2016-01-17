938ab9a0f54b5ee170085fd8337555a7b5ef85d7
Tatsuya Sen <tatsuyasen@gmail.com>
2016-01-18 00:20:12 +0900

    A

--- install2.sh
+++ install2.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 devicepath=/dev/sda
+ipaddr=
+netmask=255.255.254.0
+broadcast=
+gateway=
 
 source /etc/profile
 export PS1="(chroot) $PS1"
@@ -43,3 +47,29 @@ pip install ansible
 ansible-playbook fstab.yml --connection=local
 #ansible-playbook -i servers main.yml
 # fstab
+
+# already setted hostname="localhost"
+# vim /etc/conf.d/hostname
+#echo "config_eth0=\"${ipaddr} netmask ${netmask} brd ${broadcast}\"" > /etc/conf.d/net
+#echo "routes_eth0=\"default via ${gateway}\"" >> /etc/conf.d/net
+
+echo "config_eth0=\"dhcp\"" > /etc/conf.d/net
+
+cd /etc/init.d
+ln -s net.lo net.eth0
+rc-update add net.eth0 default
+
+# /etc/hosts
+# default
+
+# setting rootpassword
+passwd
+emerge app-admin/sysklogd
+rc-update add sysklogd default
+rc-update add sshd default
+
+emerge sys-boot/grub
+
+grub2-install ${devicepath}
+
+grub2-mkconfig -o /boot/grub/grub.cfg
